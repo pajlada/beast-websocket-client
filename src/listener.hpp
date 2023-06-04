cd313ebbe9b81c55d37f214eec6e9e798eb56ad5
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2023-06-04 14:10:36 +0200

    Add support for stream.online v1 & stream.offline v1

--- src/listener.hpp
+++ src/listener.hpp
@@ -3,6 +3,8 @@
 #include "messages/metadata.hpp"
 #include "payloads/channel-ban-v1.hpp"
 #include "payloads/session-welcome.hpp"
+#include "payloads/stream-offline-v1.hpp"
+#include "payloads/stream-online-v1.hpp"
 
 namespace eventsub {
 
@@ -17,6 +19,12 @@ public:
 
     virtual void onChannelBan(messages::Metadata metadata,
                               payload::channel_ban::v1::Payload payload) = 0;
+    virtual void onStreamOnline(
+        messages::Metadata metadata,
+        payload::stream_online::v1::Payload payload) = 0;
+    virtual void onStreamOffline(
+        messages::Metadata metadata,
+        payload::stream_offline::v1::Payload payload) = 0;
 };
 
 }  // namespace eventsub

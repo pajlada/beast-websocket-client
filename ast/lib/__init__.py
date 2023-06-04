b009181d0cfadf9d31f992baf71f77aff37b36af
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2023-06-04 12:31:43 +0200

    cleanup xd

--- ast/lib/__init__.py
+++ ast/lib/__init__.py
@@ -1,5 +1,3 @@
-from .build import build_structs
-from .format import format_code
 from .helpers import get_clang_builtin_include_dirs, init_clang_cindex, temporary_file
 from .logging import init_logging
 
